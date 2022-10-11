b494d3f8f17391a8db324f677c9f903bffe3f21c
Ricardo Alberti <ricardo.alberti.26@gmail.com>
2022-10-10 22:00:16 -0300

    datetime at newnote resolver

--- schema.js
+++ schema.js
@@ -1,10 +1,13 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  scalar DateTime
   type Note {
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
 
   type Query {
